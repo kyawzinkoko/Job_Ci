d8556c0073d0209978aaf1f2f263f8c48c629ef1
kyawzinkoko <takashikoko1995@gmail.com>
2019-07-01 17:23:33 +0630

    create

---
+++ application/views/header.php
@@ -0,0 +1,20 @@
+!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="header">
+             <div class="container">
+                <div class="row">
+                    <h1 class="display_1">Job</h1
+                </div>
+             </div>
+             <
+        </div>
+    </body>
+</html>
