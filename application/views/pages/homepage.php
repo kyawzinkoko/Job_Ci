265ced43c7203fa65e26eff989510778bcdd01f3
kyawzinkoko <takashikoko1995@gmail.com>
2019-07-02 17:01:49 +0630

    update

--- application/views/pages/homepage.php
+++ application/views/pages/homepage.php
@@ -1,1 +1,94 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+         <link href="assets/css/nav.style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="header">
+            <h1 class="display_1">Job</h1>
+        </div>
+        <div class="top">
+                <div class="row">
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                    <div class="end">
+                    <a href="#" class="btn btn-outline-success">Post</a>
+                    </div>
+                </div>
+        </div>
+        <h1> Design</h1>
+        <div class="design">
+            <table class="table table-bordered">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Location</th>
+                        <th scope="col">Position</th>
+                        <th scope="col">Company</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="col">Myanmer</th>
+                        <th scope="col">Web developer</th>
+                        <th scope="col">Aluto-benli</th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <h1>Programming</h1>
+        <div class="programming">
+            <table class="table table-bordered">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Location</th>
+                        <th scope="col">Position</th>
+                        <th scope="col">Company</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="col">Myanmer</th>
+                        <th scope="col">Web developer</th>
+                        <th scope="col">Aluto-benli</th>
+                    </tr>
+                    <tr>
+                        <th scope="col">Myanmer</th>
+                        <th scope="col">Tester</th>
+                        <th scope="col">Aluto-benli</th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+         </nav>
+        <div class="footer">
+            <nav class="nav">
+                <a class="nav-link active" href="#">Aboutjob</a>
+                <a class="nav-link" href="#">Full RssFeed</a>
+                <a class="nav-link" href="#">Job</a>
+                <a class="nav-link dis" href="#">Disabled</a>
+            </nav>
+        </div>
+   </body>
+</html>
